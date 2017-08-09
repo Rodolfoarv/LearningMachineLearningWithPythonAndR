dafa86905326da69a048707c8030c243fb1bb88f
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2017-08-09 15:37:55 -0500

    Finished Chapter 5 Bootstrap and Resampling methods

--- ProgrammingWithR/ISLR/Chapter5/Chapter5.R
+++ ProgrammingWithR/ISLR/Chapter5/Chapter5.R
@@ -71,3 +71,11 @@ boot(Portfolio, alpha.fn, R=1000)
 
 # Estimating the accuracy of the model
 
+boot.fn = function(data,index)
+  return (coef(lm(mpg~horsepower, data=data, subset=index)))
+
+boot.fn(Auto, 1:392)
+set.seed(1)
+boot.fn(Auto, sample(392,392,replace=T))
+
+boot(Auto, boot.fn, 1000)
